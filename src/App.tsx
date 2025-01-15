fe796a937d5d83a4c4f5a291fcab3a10d498800f
ElkaMaclaud <elkamaclaud@gmail.com>
2025-01-15 16:46:41 +0300

    fix: changed title

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,48 @@ import FilterField from "./components/FilterField/FilterField";
 import GoodsWrapper from "./components/GoodsWrapper/GoodsWrapper";
 import { Pagination } from "./UI_Component";
 
-
 function App() {
   const { data, loading } = useAppSelector((state) => state.page);
   const dispatch = useAppDispatch();
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    dispatch(GET_IDS())
-    dispatch(GET_FIELDS("brand"))
-    dispatch(GET_FIELDS("price"))
-  }, [])
+    dispatch(GET_IDS());
+    dispatch(GET_FIELDS("brand"));
+    dispatch(GET_FIELDS("price"));
+  }, []);
   useEffect(() => {
     if (data.ids.length) {
-        dispatch(GET_ITEMS(data.ids.slice((currentPage - 1) * 50, currentPage * 50)));
+      dispatch(
+        GET_ITEMS(data.ids.slice((currentPage - 1) * 50, currentPage * 50))
+      );
     } else {
-        dispatch(SET_GOODS([]))
+      dispatch(SET_GOODS([]));
     }
-  }, [data.ids, currentPage])
+  }, [data.ids, currentPage]);
 
   if (loading == "COMPLICATED") {
     return (
       <div className="App">
         <div className="pageWrapper">
-          <div className="filter"><FilterField /></div>
+          <div className="filter">
+            <FilterField />
+          </div>
           <div className="goods">
             <GoodsWrapper />
-            {data.goods.length > 0 && <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} data={data.ids} />}
+            {data.goods.length > 0 && (
+              <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                data={data.ids}
+              />
+            )}
           </div>
         </div>
       </div>
     );
   } else {
-    return <LoadingPage />
+    return <LoadingPage />;
   }
 }
 
